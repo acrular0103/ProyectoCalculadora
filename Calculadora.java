d87542f68f4469c2435adbf43baf568755e7e7f3
Álvaro C <alvaro.cruz@iescelia.org>
2024-05-25 20:20:26 +0200

    Implementada la funcionalidad multiplicar

--- Calculadora.java
+++ Calculadora.java
@@ -1,31 +1,37 @@
-// Clase Calculadora
 public class Calculadora {
-    // Atributos
     private int numero1;
     private int numero2;
 
-    // Constructor
     public Calculadora(int numero1, int numero2) {
         this.numero1 = numero1;
         this.numero2 = numero2;
     }
 
-    // Método para realizar una operación
     public int realizarOperacion(String operacion) {
         switch (operacion.toLowerCase()) {
             case "suma":
                 return suma();
+            case "resta":
+                return resta();
+            case "multiplicar":
+                return multiplicar();
             default:
                 throw new IllegalArgumentException("Operación no soportada: " + operacion);
         }
     }
 
-    // Método para realizar la suma
     private int suma() {
         return numero1 + numero2;
     }
 
-    // Métodos getter para los atributos (opcional, por si los necesitas más adelante)
+    private int resta() {
+        return numero1 - numero2;
+    }
+
+    private int multiplicar() {
+        return numero1 * numero2;
+    }
+
     public int getNumero1() {
         return numero1;
     }
